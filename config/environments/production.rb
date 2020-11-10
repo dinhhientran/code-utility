da42ebba9ff2df85a91d3c9b1348f1f8c1a76d66
Hien Tran <dinhhiencit@gmail.com>
2020-11-10 17:35:55 +0700

    Update Dotenv for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts << 'codeutility.io'
+  config.hosts << /[a-z0-9]+\.codeutility\.io/
+
 end
